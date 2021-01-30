e88239f87b85986429f345f87bacc680326fddd4
Rahul Ramteke <rahul24ramteke@gmail.com>
2021-01-30 12:57:49 +0530

    update contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,87 +1,66 @@
-import React, { Component } from "react";
-import Button from "./Button";
+import React, {Component} from 'react';
+import Header from './Header';
+import Button from './Button';
 import "./Contact.css";
-import ContactInfo from "./ContactInfo";
-import Header from "./Header";
-import Footer from "./Footer";
-import Image from "./assets/logo.jpg";
+import Footer from './Footer';
+
 
 class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      phone: null,
-      message: "",
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            contact:"",
+            email:"",
+            response:""
+        };
+        this.handleChange= this.handleChange.bind(this);
     };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-    this.setState({ [name]: value });
-  };
-  render() {
-    return (
-      <div className="contactContainer">
-        <Header />
-        <header className="contactHeader">
-          <h1>Contact Us</h1>
-          <div className="barid"></div>
-        </header>
-        <div className="contactBody">
-          <div className="contactInfo">
-            <ContactInfo />
-            <img className="header__logoImage contact_image" src={Image} alt="twn-logo" />
-          </div>
-          <div className="contactBox">
-            <h2 className="contactSubtitle">Reach Us Anytime.</h2>
-            <form className="contactForm">
-              <input
-                onChange={this.handleChange}
-                value={this.state.name}
-                type="text"
-                name="name"
-                placeholder="Name"
-                className="contactInput"
-              />
-              <input
-                onChange={this.handleChange}
-                value={this.state.email}
-                type="email"
-                name="email"
-                placeholder="Email"
-                className="contactInput"
-              />
-              <input
-                onChange={this.handleChange}
-                value={this.state.phone}
-                type="text"
-                maxLength="10"
-                name="contact"
-                placeholder="Contact Number"
-                className="contactInput"
-              />
-              <textarea
-                onChange={this.handleChange}
-                row="20"
-                value={this.state.message}
-                type="text"
-                name="message"
-                placeholder="Your Message"
-                className="contactInput contactMessage"
-              />
-              <div className="contactButton">
-                <Button buttonName="Contact Us" />
-              </div>
-            </form>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
-}
+
+    handleChange(e) {
+      //  var stateName=e.target.name;
+    
+        this.setState({address:e.target.value})
+    }
+
+    render(){
+        return (
+            <div>
+            <Header />
+            <div className="contact" >
+                <div className="inputSection" >
+                    <input type="text" name="name" value={this.state.name} className="form__input" autoComplete="off" required onChange={this.handleChange} />
+                    <label for ="name" className="form__label" >
+                        <span className="form__content" >Name</span>
+                    </label>
+                </div>
+                <div className="inputSection" >
+                    <input type="text" maxLength="10" name="contact" value={this.state.contact} className="form__input" autoComplete="off" required />
+                    <label for ="contact" className="form__label" >
+                        <span className="form__content" >Contact</span>
+                    </label>
+                </div>
+                <div className="inputSection" >
+                    <input type="email" name="email" value={this.state.email} className="form__input" autoComplete="off" required />
+                    <label for ="email" className="form__label" >
+                        <span className="form__content" >Email Id</span>
+                    </label>
+                </div>
+                <div className="inputSectionResponse" >
+                    <textarea rows="3" cols="48" maxLength="100" name="response" value={this.state.address} className="form__inputResponse" autoComplete="off" required onChange={this.handleChange} />
+                    <label for ="response" className="form__labelResponse" >
+                        <span className="form__content" >Response</span>
+                    </label>
+                </div>
+                <div  >
+                    <Button buttonName="Contact"  />
+                </div> 
+            </div>
+            <Footer />
+            </div>
+        );
+    };
+};
+
 
 export default Contact;
