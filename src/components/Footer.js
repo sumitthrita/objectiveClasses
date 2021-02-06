2cd00b5671500dfe7f1190f0c72be2f37b941ffe
Rahul Ramteke <rahul24ramteke@gmail.com>
2021-02-06 18:56:27 +0530

    change social icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Image from './assets/logo.jpg';
 import { Navbar } from "react-bootstrap";
-import Social from './Social';
+import YoutubeIcon from "@material-ui/icons/YouTube";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+// import Social from './Social';
 import './Footer.css';
 
 
@@ -21,7 +25,32 @@ const Footer = () => (
                 </p>
             </div>
         </div>
-        <Social />
+
+        <div className="footer__social">
+            <ul class="footer__socialList">
+                <li>
+                    <a href="https://www.instagram.com/objectiveclasses/?hl=en">
+                        <InstagramIcon />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCE98fUsVj8JJWGjHCSkHjPg">
+                        <YoutubeIcon />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/thewheelsnation?s=09">
+                        <TwitterIcon />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/objectiveclassesedu">
+                        <FacebookIcon />
+                    </a>
+                </li>
+            </ul>
+        </div>
+        {/* <Social /> */}
         <div className="footer__legal">
             <ul className="footer__legalList">
                 <li>
